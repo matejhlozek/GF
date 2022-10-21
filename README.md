3f91b8f201542118603d8dca266c8d103035f309
Hlozek <matej.hlozek@accenture.com>
2022-10-21 17:11:59 +0200

    sf stuff

--- README.md
+++
@@ -1,3 +0,0 @@
-[mygit-lesson-repository](https://github.com/matejhlozek/git-lesson-repository.git)
-[my blog webpage repository](https://github.com/matejhlozek/matejhlozek.github.io.git)
-
--- week-02/day01/Third/src/com/company/Third.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Third {
-
-    public static void main(String[] args) {
-    int [] numbers = {4, 5, 6, 7};
-        System.out.println(numbers[2]);
-    }
-}
